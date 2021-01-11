c879f431286bf8e52ccfce54a9392e9d39f37a34
贾亮亮 <deane163@126.com>
2021-01-11 20:45:25 +0800

    【文档】 添加公共功能模块

---
+++ xiaoshu-heartbeat-common/src/main/java/com/xiaoshu/XiaoshuHeartbeatCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoshuHeartbeatCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoshuHeartbeatCommonApplication.class, args);
+    }
+
+}
