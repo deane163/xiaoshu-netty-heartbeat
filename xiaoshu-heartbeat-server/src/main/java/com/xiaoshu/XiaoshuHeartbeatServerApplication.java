8dd33f56cfa9616c5f8e31b3486043479db10675
贾亮亮 <deane163@126.com>
2021-01-11 09:44:23 +0800

    first commit

---
+++ xiaoshu-heartbeat-server/src/main/java/com/xiaoshu/XiaoshuHeartbeatServerApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoshuHeartbeatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoshuHeartbeatServerApplication.class, args);
+    }
+
+}
