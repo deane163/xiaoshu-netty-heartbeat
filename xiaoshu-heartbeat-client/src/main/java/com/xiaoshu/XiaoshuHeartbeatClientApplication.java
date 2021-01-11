9061a12fc7db47ea8af0a2753597da4749b4428f
贾亮亮 <deane163@126.com>
2021-01-11 09:55:43 +0800

    【文档】 添加功能注释信息

--- xiaoshu-heartbeat-client/src/main/java/com/xiaoshu/XiaoshuHeartbeatClientApplication.java
+++ xiaoshu-heartbeat-client/src/main/java/com/xiaoshu/XiaoshuHeartbeatClientApplication.java
@@ -3,6 +3,15 @@ package com.xiaoshu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 功能说明： 心跳服务的客户端，跟服务器端保持心跳连接，如果心跳异常，则上报信息；
+ *
+ * @ com.xiaoshu
+ * <p>
+ * Original @Author: deane.jia-贾亮亮,@2021/01/11@09:59
+ * <p>
+ * Copyright (C)2012-@2021 小树盛凯科技 All rights reserved.
+ */
 @SpringBootApplication
 public class XiaoshuHeartbeatClientApplication {
 
--- xiaoshu-heartbeat-server/src/main/java/com/xiaoshu/XiaoshuHeartbeatServerApplication.java
+++ xiaoshu-heartbeat-server/src/main/java/com/xiaoshu/XiaoshuHeartbeatServerApplication.java
@@ -3,6 +3,16 @@ package com.xiaoshu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 功能说明： 使用Netty 进行 心跳传递操作；（通过集成netty框架实现心跳的统计）
+ * 监控功能：如果有客户端的心跳异常，可以及时通知到该服务端；
+ *
+ * @ com.xiaoshu
+ * <p>
+ * Original @Author: deane.jia-贾亮亮,@2021/01/11@09:32
+ * <p>
+ * Copyright (C)2012-@2021 小树盛凯科技 All rights reserved.
+ */
 @SpringBootApplication
 public class XiaoshuHeartbeatServerApplication {
 
