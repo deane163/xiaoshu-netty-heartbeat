b1367c18f159625987245e8ce0cfff0b4b8f3cdf
贾亮亮 <deane163@126.com>
2021-01-13 10:02:24 +0800

    【文档】 添加功能注释信息

--- xiaoshu-heartbeat-common/src/main/java/com/xiaoshu/XiaoshuHeartbeatCommonApplication.java
+++ xiaoshu-heartbeat-common/src/main/java/com/xiaoshu/XiaoshuHeartbeatCommonApplication.java
@@ -3,6 +3,15 @@ package com.xiaoshu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 功能说明： 心跳服务公共服务类定义；
+ *  *
+ * @ com.xiaoshu
+ * <p>
+ * Original @Author: deane.jia-贾亮亮,@2021/01/11@20:42
+ * <p>
+ * Copyright (C)2012-@2021 小树盛凯科技 All rights reserved.
+ */
 @SpringBootApplication
 public class XiaoshuHeartbeatCommonApplication {
 
